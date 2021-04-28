78290a60f1fea3e44513130d019a79c2dcdbb0c6
komal@komal405.com <komal.jadhav0542@gmail.com>
2021-04-28 12:45:50 +0530

    ADD:create contacts

--- AddressBookSystem.java
+++ AddressBookSystem.java
@@ -1,7 +1,30 @@
-Class AddressBookSystem
+class Contact 
 {
-public static void main(String args[])
-{
-System.out.println("Welcome to AddressBookSystem");
+	String Firstname;
+	String Lastname;
+	String Address;
+	String City;
+	int ZipCode;
+	int PhoneNumber;
+	String Email;
+	
+	Contact(String FirstName,String LastName,String Address,String City, int ZipCode,int PhoneNumber,String Email)
+	{
+		this.Firstname = FirstName;
+		this.Lastname = LastName;
+		this.Address = Address;
+		this.City = City;
+		this.ZipCode = ZipCode;
+		this.PhoneNumber = PhoneNumber;
+		this.Email = Email
+
+	}
 }
+
+class AddressBook
+{
+	public static void main(String args[])
+	{
+		System.out.println("Welcome to Address Book");
+	}
 }
