2a374443dfb77dcc7ea36d7030e31b013e17c98a
komal@komal405.com <komal.jadhav0542@gmail.com>
2021-04-28 16:24:10 +0530

    ADD:Add new contacts

--- AddressBookSystem.java
+++ AddressBookSystem.java
@@ -19,12 +19,49 @@ class Contact
 		this.Email = Email
 
 	}
+void Display()
+		{
+		System.out.println("First Name is :" + Firstname);
+		System.out.println("Last Name is :" + Lastname);
+		System.out.println("Address is :" + Address);
+		System.out.println("Name of city is :" + City);
+		System.out.println("ZipCode is :" + ZipCode);
+		System.out.println("Contact number is :" + PhoneNumber);
+		System.out.println("Email id is :" + Email);
+		}
 }
-
-class AddressBook
+class AddressBookInfo
 {
-	public static void main(String args[])
+	public static void main (String args[])
 	{
-		System.out.println("Welcome to Address Book");
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Enter First Name:");
+		String Firstname=scan.nextLine();
+		
+		System.out.println("Enter Last Name:");
+		String Lastname=scan.nextLine();
+		
+		System.out.println("Enter Address:");
+		String Address=scan.nextLine();
+		
+		System.out.println("Enter City Name:");
+		String City=scan.nextLine();
+		
+		System.out.println("Enter ZipCode :");
+		int ZipCode=scan.nextInt();
+		
+		System.out.println("Enter ContactNumber:");
+		Long PhoneNumber =scan.nextLong();
+		
+		System.out.println("Enter Email id:");
+		String Email =scan.nextLine();
+	
+		System.out.println("Welcome to Address Book system");
+		ContactInfo c = new ContactInfo();
+		c.setContactInfo(Firstname,Lastname,Address,City,ZipCode,PhoneNumber,Email);
+		c.Display();
 	}
 }
+
+
